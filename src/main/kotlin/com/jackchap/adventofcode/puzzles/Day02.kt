fb0a3cb40a0c250f13e0bfd70d82a0cb2c0db232
crepppy <chapmanjack0777@gmail.com>
2022-12-05 01:52:36 +0000

    ✍🏻 Refactor Day Two

--- src/main/kotlin/com/jackchap/adventofcode/puzzles/Day02.kt
+++ src/main/kotlin/com/jackchap/adventofcode/puzzles/Day02.kt
@@ -1,33 +1,38 @@
 package com.jackchap.adventofcode.puzzles
 
-typealias StrategyGuide = List<Pair<Char, Char>>
+typealias StrategyGuide = List<Pair<Sign, Sign>>
 
 object Day02 : Day<StrategyGuide>(2, "Rock Paper Scissors") {
     override fun parseInput(input: String) =
-        input.lines().map { it.toCharArray().let { c -> c[0] + 23 to c[2] } }
+        input.lines().map { it.toCharArray().let { c -> Sign.values()[c[0] - 'A'] to Sign.values()[c[2] - 'X'] } }
 
-    private fun winningChar(char: Char): Char = when (char) {
-        'X' -> 'Y'
-        'Y' -> 'Z'
-        'Z' -> 'X'
-        else -> throw IllegalStateException()
-    }
-
-    override fun partOne(input: StrategyGuide) = input.fold(0) { acc, (theirs, response) ->
-        acc + (response - 'W') + when (response) {
+    override fun partOne(input: StrategyGuide) = input.sumOf { (theirs, response) ->
+        response.score + when (response) {
             theirs -> 3
-            winningChar(theirs) -> 6
+            theirs.winningSign -> 6
             else -> 0
         }
     }
 
     override fun partTwo(input: StrategyGuide) = partOne(input.map {
         it.first to when (it.second) {
-            'X' -> winningChar(winningChar(it.first))
-            'Y' -> it.first
-            'Z' -> winningChar(it.first)
-            else -> throw IllegalStateException()
+            Sign.PAPER -> it.first
+            Sign.SCISSORS -> it.first.winningSign
+            Sign.ROCK -> it.first.winningSign.winningSign
         }
     })
 
+}
+
+enum class Sign(val score: Int) {
+    ROCK(1),
+    PAPER(2),
+    SCISSORS(3);
+
+    val winningSign
+        get() = when(this) {
+            ROCK -> PAPER
+            PAPER -> SCISSORS
+            SCISSORS -> ROCK
+        }
 }
